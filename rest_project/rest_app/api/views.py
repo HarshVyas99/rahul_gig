19eb84273376f966edbbc2235a24f3ee70de751f
Harsh <harshvyas9990@gmail.com>
2024-03-23 21:20:38 +0530

    Code update

--- rest_project/rest_app/api/views.py
+++ rest_project/rest_app/api/views.py
@@ -5,9 +5,23 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_app.script import main
 import json
+from json import JSONEncoder
 import logging
 
 debug_logger = logging.getLogger('debug_logger')
+
+
+class CustomEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            # If the object is bytes, decode it to string
+            return obj.decode('utf-8', errors='replace')
+        elif isinstance(obj, str):
+            # If the object is already string, return it as is
+            return obj
+        else:
+            # For other types, return their default JSON representation
+            return super().default(obj)
 
 class VerificationRequestCreateView(CreateAPIView):
     serializer_class = VerificationRequestSerializer
@@ -29,7 +43,7 @@ class VerificationRequestCreateView(CreateAPIView):
                 request_id=serializer.data['id']
                 obj=VerificationRequest.objects.filter(id=request_id).first()
                 obj.request_status=response['status']
-                obj.request_response=json.dumps(response)
+                obj.request_response = json.dumps(response, cls=CustomEncoder)
                 obj.save(update_fields=['request_status','request_response'])
                 serializer=self.get_serializer(obj)
                 headers = self.get_success_headers(serializer.data)
